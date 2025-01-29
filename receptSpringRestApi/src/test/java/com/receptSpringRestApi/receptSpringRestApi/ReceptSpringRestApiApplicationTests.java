10e1bdac3607beca3c832a8b397137ebf9372db9
Fatema Afroza <fafroza2017@gmail.com>
2025-01-29 11:43:49 +0100

    fist commit

---
+++ receptSpringRestApi/src/test/java/com/receptSpringRestApi/receptSpringRestApi/ReceptSpringRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.receptSpringRestApi.receptSpringRestApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceptSpringRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
